cddbf9082f62fc46e779f649b3affacea75eef29
elisakis <elisa.kis@yandex.ru>
2017-01-11 03:11:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,34 +23,36 @@ int Continuous_knapsack(int W, int n)
         int j;
         for (j = i - 1; j >= 0 && arr_temp[j] < eff1; j--)  // поиск места элемента в готовой последовательности
         {
-            arr_temp[j+1] = arr_temp[j];  // сдвигаем элемент направо, пока не дошли
-            arr_sort[j+1] = arr_sort[j];
+            arr_temp[j+1] = arr_temp[j];  // сдвигаем элемент направо, пока не дошли (сортируем временный массив)
+            arr_sort[j+1] = arr_sort[j];  // запоминаем новый порядок сортировки
         }
 
         arr_temp[j + 1] = eff1; // место найдено, вставить элемент (или i == 0)
-        arr_sort[j + 1] = i;
+        arr_sort[j + 1] = i;    // запоминаем новый порядок сортировки
     }
 
     price_all = 0;
     W_all = 0;
     double temp;
+    //заполняем рюкзак
     for (int i = 0; i < n; i++)
     {
-        if (W_all + arr_W[arr_sort[i]] <= W)
+        if (W_all + arr_W[arr_sort[i]] <= W)    //с учетом новой сортирвки по убыванию эффективность предмета (цена единицы объема)
         {
             price_all += arr_price[arr_sort[i]];
             W_all += arr_W[arr_sort[i]];
-            arr_temp[arr_sort[i]] = 1.0;  //запомним долю предмета
+            arr_temp[arr_sort[i]] = 1.0;  //запомним долю предмета во временном массиве
         }
         else
-        {
-            part1 = (W - W_all) / arr_W[arr_sort[i]];
+        {   //не влезло
+            part1 = (W - W_all) / arr_W[arr_sort[i]]; //часть предмета
             price_all += part1 * arr_price[arr_sort[i]];
             W_all += part1 * arr_W[arr_sort[i]];
-            arr_temp[arr_sort[i]] = part1;  //запомним долю предмета
+            arr_temp[arr_sort[i]] = part1;  //запомним долю предмета во временном массиве
         }
     }
-
+    
+    //выводим доли
     for (int i = 0; i < n; i++)
     {
         cout << arr_temp[i] << " ";
